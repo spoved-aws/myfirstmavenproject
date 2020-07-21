56be1e7d4c10f291d4b1b20751d207c1d2782f88
kanukhosla <kanukhosla10@gmail.com>
2020-07-21 15:07:35 -0700

    variable fix

--- src/test/java/com/practiceTestClasses/CheckingStaticVariables.java
+++ src/test/java/com/practiceTestClasses/CheckingStaticVariables.java
@@ -9,28 +9,28 @@ import com.utilitiesPractice.Listener_CheckingStaticVariables;
 
 @Listeners(Listener_CheckingStaticVariables.class)
 public class CheckingStaticVariables extends Base_Practice {
-	
+
 	@BeforeMethod
 	public void setUp() {
-		
+
 		initialization();
 	}
-	
-	
+
+
 	@Test
 	public void changeTheValueOf_i() {
-		
-		System.out.println("The value of i before changing : " +i);
+
+		/*System.out.println("The value of i before changing : " +i);
 		System.out.println("***************************************");
 		i=30;
 		System.out.println("The value of i after changing : " +i);
 		System.out.println("***************************************");
-		
-		
-	}
-	
-	
+		*/
 
-	
-	
+	}
+
+
+
+
+
 }
